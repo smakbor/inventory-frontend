c587b66c2f4058febab43e19553d0df66e7d9f2d
shamim <smakbor24@gmail.com>
2024-04-23 01:39:18 +0600

    shamim comment

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,75 +2,75 @@
 import React from "react";
 
 import {
-    AppBar,
-    Drawer,
-    List,
-    ListItem,
-    ListItemText,
-    Toolbar,
-    Typography,
+  AppBar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import BasicTable from "./Table";
+import Products from "./components/products/Products";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-    },
-    appBar: {
-        zIndex: theme.zIndex?.drawer + 1,
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    content: {
-        flexGrow: 1,
-        // padding: theme?.spacing(3),
-    },
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex?.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    // padding: theme?.spacing(3),
+  },
 }));
 
 const Sidebar = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar>
-                    <Typography variant="h6" noWrap>
-                        My Dashboard
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes?.drawer}
-                variant="permanent"
-                classes={{
-                    paper: classes?.drawerPaper,
-                }}
-                anchor="left"
-            >
-                <List>
-                    {["Dashboard", "Product", "Brand", "Unit", "Category"].map(
-                        (text) => (
-                            <ListItem button key={text}>
-                                <ListItemText primary={text} />
-                            </ListItem>
-                        )
-                    )}
-                </List>
-            </Drawer>
-            <main className={classes.content}>
-                <Toolbar />
-                <BasicTable />
-            </main>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            My Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes?.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes?.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <List>
+          {["Dashboard", "Product", "Brand", "Unit", "Category"].map((text) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <BasicTable />
+        <Products />
+      </main>
+    </div>
+  );
 };
 
 export default Sidebar;
